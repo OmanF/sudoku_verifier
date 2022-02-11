820f9e837b8e9173992b300493d73102997f2a19
O.F.K <ohadfjunkbox@outlook.com>
2022-02-11 20:21:18 +0200

    Sudoku verifier - complete

--- Program.fs
+++ Program.fs
@@ -1,2 +1,18 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open Verifier
+
+[<EntryPointAttribute>]
+let main _ =
+    let input =
+        "123456789123456789123456789123456789123456789123456789123456789123456789123456789"
+
+    let rows = input |> parseInput |> getRows
+    let cols = input |> parseInput |> getColumns
+
+    let ``rowsValid?`` = rows |> validateAllEntities
+    let ``colsValid?`` = cols |> validateAllEntities
+
+    match (``rowsValid?`` && ``colsValid?``) with
+    | false -> printfn "Solution is invalid"
+    | true -> printfn "Solution is valid"
+
+    0
--- Verifier.fs
+++ Verifier.fs
@@ -1,17 +1,6 @@
 module Verifier
 
-type EntityType =
-    | Row
-    | Column
-    | Box
-
-type EntityValidity =
-    | Valid
-    | IncorrectRow of string
-    | IncorrectColumn of string
-    | IncorrectBox of string
-
-let isValidEntity entity =
+let ``isValidEntity?`` entity =
     // A valid sudoku entity (i.e., row, column or 3x3 cube) **must** contain all numbers 1 through 9, inclusive
     let validSet = set [ 1; 2; 3; 4; 5; 6; 7; 8; 9 ]
     let entitySet = set entity
@@ -25,23 +14,20 @@ let parseInput (input: string) =
     |> Array.map (fun arr -> Array.map (fun elem -> int elem - int '0') arr) // Convert the input string to array-of-arrays, each 9 elements longs, all `int`s. The `- int '0'` is to get the true value of the **char** and **not** its ASCII value!
     |> array2D
 
-let solutionRows (parsedInput: int [,]) =
+let getRows (parsedInput: int [,]) =
     [| for i in 0 .. (parsedInput.GetLength(0) - 1) -> parsedInput[i, *] |]
 
-let solutionColumns (parsedInput: int[,]) =
+let getColumns (parsedInput: int[,]) =
     [| for i in 0 .. (parsedInput.GetLength(1) - 1) -> parsedInput[*, i] |]
 
-let solutionBoxes solutionRows =
-    let interim = solutionRows |> Array.collect (Array.chunkBySize 3)
-    // TODO: Implment
+// let getBoxes solutionRows =
+//     let chunkSize = 3 // No "magic numbers" here. 3 due to the boxes each being 3 X 3 in size
+//     let interim = solutionRows |> Array.collect (Array.chunkBySize chunkSize)
+//     [| for i in 0 .. ((Array.length interim / chunkSize) - 1) -> interim[0 + (chunkSize * i)]|]
 
-let validateAllEntities (entities: int array array) (entityType: EntityType) =
-    Array.map isValidEntity entities
+let validateAllEntities (entities: int array array) =
+    Array.map ``isValidEntity?`` entities
     |> Array.contains false
     |> function
-        | false -> Ok Valid
-        | true ->
-            match entityType with
-            | Row -> Error(IncorrectRow "At least one row is incorrect")
-            | Column -> Error(IncorrectColumn "At least one column is incorrect")
-            | Box -> Error(IncorrectBox "At least one box is incorrect")
+        | false -> false
+        | true -> true
